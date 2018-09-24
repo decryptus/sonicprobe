969e021c6e1e50cafbf39ac2a31eb2e7c00bad3c
Adrien Delle Cave <adrien.delle.cave@commandersact.com>
2018-09-24 09:04:56 +0200

    [RELEASE] version: 0.2.53

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 from setuptools import find_packages, setup
 
 requirements = [line.strip() for line in open('requirements.txt', 'r').readlines()]
-version      = '0.2.52'
+version      = '0.2.53'
 
 if os.path.isfile('VERSION'):
     version = open('VERSION', 'r').readline().strip() or version
